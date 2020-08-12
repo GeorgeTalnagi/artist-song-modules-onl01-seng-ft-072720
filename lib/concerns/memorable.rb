4ceebaa5a52b8bb4a2cae5f2c1c04065e762f739
George Talnagi <gtalnagi@yahoo.com>
2020-08-12 21:00:23 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable 
-
+  module ClassMethods
   def reset_all
     all.clear
   end
 
   def count
     all.count
+  end
+  
+  module InstanceMethods
+    def initialize
+      # some more code coming soon!
+    end
   end
 
 
